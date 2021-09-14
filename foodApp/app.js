bf713430e3b61eabfee96f6d03627bba109c88a6
SOURABH <dubeysourabh2001@gmail.com>
2021-09-14 16:33:26 +0530

    Backend

---
+++ foodApp/app.js
@@ -0,0 +1,32 @@
+//npm init -y
+// npm install express
+// npm i nodemon -g
+
+const express=require('express');
+
+//server creation
+const app=express();
+let port='8081';
+app.listen(port,function(){
+    console.log(`server is listening on port ${port} `);
+});
+//types of request -> get post put delete 
+app.get('/',(req,res)=>{
+    console.log(req.hostname);
+    console.log(req.path);
+    console.log(req.method);
+    console.log('hello from home page');
+    res.send('<h1>hello hi  </h1>');
+});
+let obj={
+    'name':'abcde'
+}
+app.get('/user',(req,res)=>{
+    console.log(' hi users');
+    res.json(obj);
+});
+
+app.get('/home',(req,res)=>{
+    console.log(__dirname);
+    res.sendFile('./views/index.html',{root:__dirname});
+})
