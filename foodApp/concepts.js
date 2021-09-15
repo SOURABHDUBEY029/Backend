7b85beb7726268a11fa5cf26b0530f6668192efa
SOURABH <dubeysourabh2001@gmail.com>
2021-09-16 02:52:58 +0530

    forgotPassword

--- foodApp/concepts.js
+++ foodApp/concepts.js
@@ -6,11 +6,11 @@ app.listen('5000',function(){
 });
 
 app.use(express.json());
-app.use((req,res,next)=>{
-    //do some work
-    console.log('middleware function');
-    next();
-});
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('middleware function');
+//     next();
+// });
 
 app.use(express.static('public'));
 const userRouter = express.Router();
@@ -26,11 +26,11 @@ userRouter
 .patch(updateUser)
 .delete(deleteUser);
 
-app.use((req,res,next)=>{
-    //do some work
-    console.log('middleware 2nd time');
-    next();
-})
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('middleware 2nd time');
+//     next();
+// })
 
 userRouter
 .route('/:id')
@@ -39,6 +39,37 @@ userRouter
 authRouter
 .route('/signup')
 .post(signupUser);
+
+authRouter
+.route('/forgotPassword')
+.get(getForgotPassword)
+.post(postForgotPassword,validateEmail);
+
+function getForgotPassword(req,res){
+    res.sendFile('./public/forgotPassword.html',{root:__dirname});
+}
+
+function postForgotPassword(req,res,next){
+    let data = req.body;
+    console.log('data', data);
+    //check if email id is correct - validate
+    next();
+    //check if user exist in db
+    // res.json({
+    //     message: "data recieved",
+    //     data: data.email
+    // })
+};
+
+function validateEmail(req,res){
+    console.log('validate Email function');
+    console.log(req.body);
+    //check if email is correct or not
+    res.json({
+        message: "data recieved",
+        data: req.body
+    })
+}
 
 function signupUser(req,res){
     // let userDetails = req.body;
